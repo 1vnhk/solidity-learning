5b136901e74a6cbee1d6ff19bdfbd9e28b607a3b
1vnhk <iandrc9@gmail.com>
2026-02-15 18:05:42 +0100

    feat: add store and getter

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,5 +2,13 @@
 pragma solidity >=0.8.33 <0.9.0;
 
 contract SimpleStorage {
-    uint256 favoriteNumber;
+    uint256 public favoriteNumber; // default is 0
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
 }
