29a8c58317144a5d231cf694ab417c481175f086
1vnhk <iandrc9@gmail.com>
2026-02-18 21:49:00 +0100

    feat: interact with SimpleStorage ABI

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -4,9 +4,19 @@ pragma solidity ^0.8.24;
 import { SimpleStorage } from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _newSimpleStorageIndex, uint256 _newFavoriteNumber) public {
+        // ABI
+        listOfSimpleStorageContracts[_newSimpleStorageIndex].store(_newFavoriteNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
     }
 }
